2b69e55cee1943f6e374e69c5224507af3aa857f
I-am-Byte <victor.olaomo@outlook.com>
2025-12-04 11:51:33 +0100

    latest

---
+++ PYTHON SNACKS/PetChooser.py
@@ -0,0 +1,11 @@
+name=input("Kindly enter your Name: ").lower()
+
+
+if name.isdigit() == True:
+   print("Invalid Input, Please") 
+else:
+    if name=="alex":
+        print("Hello, friend")
+    
+    else:
+        print("Hi stranger") 
